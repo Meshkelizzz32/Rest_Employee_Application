4c68ecbad0f6f376d96022b7dd9ab570c1b288d8
Meshkelizzz32 <meshkeliszzz@gmail.com>
2024-02-12 11:28:51 +0300

    Rest_employee_app

---
+++ spring_course_rest/src/main/java/com/Meshkel/spring/rest/exception_handling/EmployeeIncorrectData.java
@@ -0,0 +1,16 @@
+package com.Meshkel.spring.rest.exception_handling;
+
+public class EmployeeIncorrectData {
+    private String Info;
+
+    public EmployeeIncorrectData() {
+    }
+
+    public String getInfo() {
+        return Info;
+    }
+
+    public void setInfo(String info) {
+        Info = info;
+    }
+}
---
+++ spring_course_rest/src/main/java/com/Meshkel/spring/rest/exception_handling/NoSuchEmployeeException.java
@@ -0,0 +1,7 @@
+package com.Meshkel.spring.rest.exception_handling;
+
+public class NoSuchEmployeeException extends RuntimeException{
+    public NoSuchEmployeeException(String message) {
+        super(message);
+    }
+}
